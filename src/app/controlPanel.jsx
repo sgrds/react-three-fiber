715901450b5496f70f4a158ae8e32ffe542cfafc
sgrDs <sagardasdev@gmail.com>
2023-10-24 04:54:32 +0530

    damping toggle input added to controlpanel

---
+++ src/app/controlPanel.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+
+const ControlPanel = ({ controls, setControls }) => {
+  return (
+    <div className="min-w-xl absolute bottom-0 right-0 rounded-md bg-white px-6 py-4 shadow-md ">
+      <h1 className="pb-2 text-lg font-medium">Control Pannel</h1>
+      <div className="grid gap-4">
+        <label className="relative inline-flex cursor-pointer items-center">
+          <input
+            type="checkbox"
+            checked={controls.hdri}
+            onChange={() => setControls({ ...controls, hdri: !controls.hdri })}
+            className="peer sr-only"
+          />
+          <div className="peer h-6 w-11 rounded-full bg-gray-200 after:absolute after:left-[2px] after:top-[2px] after:h-5 after:w-5 after:rounded-full after:border after:border-gray-300 after:bg-white after:transition-all after:content-[''] peer-checked:bg-blue-600 peer-checked:after:translate-x-full peer-checked:after:border-white peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300"></div>
+          <span className="ml-3 text-sm font-medium text-gray-900 ">HDRI</span>
+        </label>
+        <label className="relative inline-flex cursor-pointer items-center">
+          <input
+            type="checkbox"
+            checked={controls.damping}
+            onChange={() =>
+              setControls({ ...controls, damping: !controls.damping })
+            }
+            className="peer sr-only"
+          />
+          <div className="peer h-6 w-11 rounded-full bg-gray-200 after:absolute after:left-[2px] after:top-[2px] after:h-5 after:w-5 after:rounded-full after:border after:border-gray-300 after:bg-white after:transition-all after:content-[''] peer-checked:bg-blue-600 peer-checked:after:translate-x-full peer-checked:after:border-white peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300"></div>
+          <span className="ml-3 text-sm font-medium text-gray-900 ">
+            Damping
+          </span>
+        </label>
+        <div>
+          <p>
+            press <kbd>ctrl</kbd> and move mouse to pan across the screen
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ControlPanel;
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,26 +1,32 @@
 "use client";
 import { Canvas } from "@react-three/fiber";
 import House from "./house";
-import { Stats, OrbitControls, CameraControls } from "@react-three/drei";
+import { OrbitControls } from "@react-three/drei";
 import { useState } from "react";
-import ControlPannel from "./controlPannel";
+import ControlPanel from "./controlPanel";
 
 export default function Home() {
-  const [controls, setControls] = useState({ hdri: false });
+  const [controls, setControls] = useState({ hdri: false, damping: true });
   return (
     <div className="flex h-screen w-screen items-center justify-center">
       <Canvas
         className="cursor-pointer"
         frameloop="demand"
-        camera={{ position: [0, 10, -50], fov: 30, near: 0.1, far: 200 }}
+        camera={{
+          position: [-0.02, 0.01, 0],
+          fov: 20,
+          near: 0.1,
+          far: 200,
+        }}
       >
-        {/* <OrbitControls
-          autoRotate={false}
+        <OrbitControls
+          // autoRotate={false}
+          enableDamping={controls.damping}
           enableZoom={true}
-          maxPolarAngle={Math.PI / 2}
-          minPolarAngle={Math.PI / 2}
-          enablePan={false}
-        /> */}
+          maxPolarAngle={Math.PI / 2.02}
+          maxDistance={65}
+          enablePan={true}
+        />
         <ambientLight intensity={0.5} />
         <spotLight
           intensity={0.8}
@@ -28,21 +34,12 @@ export default function Home() {
           angle={0.2}
           penumbra={1}
         />
-        <CameraControls
-          truckSpeed={0}
-          dollySpeed={0}
-          minPolarAngle={0}
-          maxPolarAngle={Math.PI / 2}
-        />
+
         <House env={controls.hdri} />
 
-        <axesHelper args={[20]} />
-        <Stats />
+        {/* <axesHelper args={[20]} /> */}
       </Canvas>
-      <ControlPannel
-        checked={controls.hdri}
-        onChange={() => setControls({ ...controls, hdri: !controls.hdri })}
-      />
+      <ControlPanel controls={controls} setControls={setControls} />
     </div>
   );
 }
