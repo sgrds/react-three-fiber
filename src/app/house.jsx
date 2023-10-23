f99c2fc69e3bbaca3f4dd9610386541efdd0a2f1
sgrDs <sagardasdev@gmail.com>
2023-10-24 04:55:53 +0530

    text added to house mesh

--- src/app/house.jsx
+++ src/app/house.jsx
@@ -1,7 +1,5 @@
 "use client";
-
-// import * as THREE from "three";
-import { useGLTF, Environment, Stage } from "@react-three/drei";
+import { useGLTF, Environment, Stage, Html } from "@react-three/drei";
 
 const House = ({ env }) => {
   const house = useGLTF(
@@ -10,19 +8,24 @@ const House = ({ env }) => {
 
   return (
     <>
-      <Stage>
-        {/* <group dispose={null}> */}
-        <primitive
-          //   position={new THREE.Vector3(0, 0, 0)}
-          object={house.scene}
-          scale={2}
-        />
-        {/* </group> */}
-      </Stage>
+      <group>
+        <Html position={[10, 2, 0]} center>
+          <h1 className="w-auto rounded-full bg-gray-700 px-8 py-4 text-white opacity-75">
+            Assignment Done..👍✨
+          </h1>
+        </Html>
+        <Stage environment="sunset">
+          <primitive position={[0, -20, 0]} object={house.scene} scale={2} />
+        </Stage>
+      </group>
       {env === true && (
         <Environment
           files="https://qpzsjmjzjfmluroetwtm.supabase.co/storage/v1/object/public/sagarcodes/3d/farm_sunset_1k.exr"
-          ground={{ height: 50, radius: 500, scale: 80 }}
+          ground={{
+            height: 32,
+            radius: 340,
+            scale: 100,
+          }}
         />
       )}
     </>
